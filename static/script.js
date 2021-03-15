00b7ce4276de2c58e92a949167e896b6f46e6227
alban.rousseau <alban.rousseau@o2.fr>
2021-03-15 18:04:00 +0100

    Modification entête, apparemment ça plait pas le papounet

--- static/script.js
+++ static/script.js
@@ -2,7 +2,6 @@ function mafonction() {
     const formulaire = document.getElementById("formulaire");
     console.log(formulaire);
     formulaire.style.display = "none";
-    console.log("Bon ça fonctionne maintenant");
     const loader = document.getElementById("loader");
     loader.style.display = "inherit";
     console.log(loader);
